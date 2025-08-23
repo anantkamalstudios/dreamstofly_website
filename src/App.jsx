2f86d81a2ca721f20241e68356e4f4da1c2afc52
RoshanAnantkamalstudios <roshan.anantkamalstudios@gmail.com>
2025-08-23 19:04:03 +0530

    rv commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./component/layouts/MainLayout";
 import Home from "./component/pages/Home";
-import "./App.css"
+import "./App.css";
 import Blogs from "./component/pages/blogs/Blogs";
 import MainModal from "./component/Loginregister/MainModal";
 import LoginRegisterPage from "./component/Loginregister/LoginRegisterPage";
-import Login from "./component/Loginregister/Login"
+import Login from "./component/Loginregister/Login";
 import Register from "./component/Loginregister/Register";
 import CourceFinder from "./component/pages/CourceFinder";
 import BlogDetails from "./component/pages/blogs/BlogDetails";
@@ -15,8 +15,6 @@ import PostAdmitServices from "./component/pages/postadmitservices/PostAdmitServ
 import ExamPrep from "./component/pages/examprep/ExamPrep";
 import FreeCourses from "./component/pages/freecourses/FreeCourses";
 import Travel from "./component/pages/travel/Travel";
-
-
 
 import ContactUs from "./component/footerpages/ContactUs";
 import Aboutus from "./component/footerpages/AboutUs";
@@ -30,14 +28,12 @@ import ToeflIBT from "./component/pages/examprep/Toefl_IBT";
 import DuolingoTest from "./component/pages/examprep/DuolingoTest";
 import GREPrep from "./component/pages/examprep/GREPrep";
 
-
 // import About from "./component/pages/";
 // import Contact from "./pages/Contact";
 
 export default function App() {
   return (
     <>
-
       <Router>
         <MainLayout>
           <Routes>
@@ -51,29 +47,25 @@ export default function App() {
             <Route path="/cource-finder" element={<CourceFinder />} />
             <Route path="/sop-maker" element={<SopMaker />} />
             <Route path="/connect" element={<Connect />} />
-            <Route path="/post-admit-services" element={<PostAdmitServices />} />
+            <Route
+              path="/post-admit-services"
+              element={<PostAdmitServices />}
+            />
             <Route path="/exam-prep" element={<ExamPrep />} />
             <Route path="/free-cources" element={<FreeCourses />} />
             <Route path="/travel" element={<Travel />} />
-            
 
-            
             <Route path="/about" element={<Aboutus />} />
-             <Route path="/career" element={<CareerPage />} />
-             <Route path="/terms" element={<TermsPage />} />
+            <Route path="/career" element={<CareerPage />} />
+            <Route path="/terms" element={<TermsPage />} />
             <Route path="/advertising" element={<Advertise />} />
             <Route path="/contact" element={<ContactUs />} />
-            <Route path="/IELTS" element={<IELTSPrep/>} />
-            <Route path="/IELTS-ACEDEMIC" element={<IeltsAcademic/>} />
-            <Route path="/PTEAcademic" element={<PTEAcademic/>} />
-            <Route path="/ToeflIBT" element={<ToeflIBT/>} />
-            <Route path="/DuolingoTest" element={<DuolingoTest/>} />
-            <Route path="/GREPrep" element={<GREPrep/>} />
-   
-
-
-
-
+            <Route path="/IELTS" element={<IELTSPrep />} />
+            <Route path="/IELTS-ACEDEMIC" element={<IeltsAcademic />} />
+            <Route path="/PTEAcademic" element={<PTEAcademic />} />
+            <Route path="/ToeflIBT" element={<ToeflIBT />} />
+            <Route path="/DuolingoTest" element={<DuolingoTest />} />
+            <Route path="/GREPrep" element={<GREPrep />} />
           </Routes>
         </MainLayout>
       </Router>
